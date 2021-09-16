3f0183e6a8279ef6a938fdfdc32bb62c98544413
itsmeaneesh1 <javaglitz@gmail.com>
2021-09-16 15:09:52 +0530

    Maven Project

---
+++ MavenWeb2/src/test/java/MyCalculatorTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.jenk.MyCalculator;
+
+public class MyCalculatorTest {
+
+	@Test
+	public void test1() {
+		assertEquals(10, new MyCalculator().getSum(5, 5));
+	}
+	@Test
+	public void test2() {
+		assertEquals(5, new MyCalculator().getSum(2, 3));
+	}
+	@Test
+	public void test3() {
+		assertEquals(5, new MyCalculator().getDiff(10,5));
+	}
+	@Test
+	public void test4() {
+		assertEquals(0, new MyCalculator().getSum(5, 5));
+	}
+
+}
